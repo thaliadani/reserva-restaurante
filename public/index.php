a2bcd6c09a7f100793a8830f389e953915c2b4ac
Thalia Danielle <31187727+thaliadani@users.noreply.github.com>
2025-10-23 11:19:23 -0300

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include '../includes/templates/header.php'; ?>
+    
+    <main>
+
+    </main>
+    
+    <?php include '../includes/templates/footer.php'; ?>
+</body>
+</html>
+
